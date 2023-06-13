915405154aee6953d37548780cb9cdc7e24833b5
Oleksandr Pronin <pronin.alx@gmail.com>
2023-06-13 20:43:43 +0200

    build: update strapi configuration

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: "https://rc-ua-strapi.captain.regionit.de",
+  proxy: true,
   app: {
     keys: env.array("APP_KEYS"),
   },
