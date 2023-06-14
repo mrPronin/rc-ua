e0187be0c1387e302dc7e93f574e790ade597773
Oleksandr Pronin <pronin.alx@gmail.com>
2023-06-14 15:39:40 +0200

    feat: strapi, define 'Blood donation center' collection

---
+++ strapi/src/api/blood-donation-center/controllers/blood-donation-center.ts
@@ -0,0 +1,7 @@
+/**
+ * blood-donation-center controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::blood-donation-center.blood-donation-center');
