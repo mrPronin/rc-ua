e8d84b860605f332f4eafb4d82d66710a714dc84
OlegGre4ka <gre4kae@ukr.net>
2023-07-16 13:39:49 +0300

    feat: create web-client with vite, introduce PWA, add documentation (#3)
    Co-authored-by: Oleksandr Pronin <pronin.alx@gmail.com>

---
+++ web-client/vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      'components': '/src/components',
+      'routes': '/src/routes',
+      'view': '/src/view'
+    },
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'RC-UA',
+        short_name: 'RC-UA',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/path/to/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/path/to/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),],
+})
