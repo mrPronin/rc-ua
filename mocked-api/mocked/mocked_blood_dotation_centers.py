cc41f10dd76d0f8e00861479d9edb94556cc0a78
Oleksandr Pronin <pronin.alx@gmail.com>
2023-08-12 16:32:52 +0200

    refactor: wrap json import code inside main function

--- mocked-api/mocked/mocked_blood_dotation_centers.py
+++ mocked-api/mocked/mocked_blood_dotation_centers.py
@@ -1,7 +1,14 @@
 import json
 
-with open("./mocked/bdc_mock_data.json", "r") as f:
-    json_string = json.load(f)
 
-blood_donation_centers = json_string.get("data").get("bloodDonationCenters")
-blood_dotation_data = blood_donation_centers.get("data")
+def load_bdc_data():
+    with open("./mocked/bdc_mock_data.json", "r") as f:
+        json_string = json.load(f)
+        blood_donation_centers = json_string.get("data").get(
+            "bloodDonationCenters"
+        )  # noqa: E501
+        return blood_donation_centers.get("data")
+
+
+if __name__ == "__main__":
+    bdc_data = load_bdc_data()
