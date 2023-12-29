ba078f672499e780f439f74013f4ddc61339c8fa
Oleksandr Pronin <pronin.alx@gmail.com>
2023-12-29 12:40:41 +0100

    api:feature: CORS disable all origins

--- api-server/src/api/app.py
+++ api-server/src/api/app.py
@@ -7,13 +7,13 @@ graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
 origins = [
-    "*",
-    # "http://localhost:5173/"
+    # "*",
+    "http://localhost:5173/"
 ]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
     max_age=3600
